58649388864d30567acd878d6b2260df149ec815
trangtruong21 <annatruong345@gmail.com>
2021-11-26 19:13:44 +0700

    update

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(window).scroll(function () {
+    let oTop = $('.section-2').offset().top - window.innerHeight;
+    if($(window).scrollTop() > oTop) {
+           $('.navbar').addClass('sticky');
+    } else {
+           $('.navbar').removeClass('sticky');
+    }
+})
